26c1d1a6c2ef0885f3db2d44255085be5595b242
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-08-11 13:00:31 +0200

    feat: toevoegen van initieel schema

---
+++ src/databank/schema.sql
@@ -0,0 +1,64 @@
+create table categorie (
+	id varchar primary key,
+	naam varchar unique not null, /* Zou uniek moeten zijn */
+	alt_naam varchar,
+	
+	is_weez bool default true,
+	_laatste_sync timestamp default NOW()
+	
+);
+
+create table tarief (
+	id varchar primary key
+	naam varchar, /* Potentieel constraint met ID dat combo uniek is */
+	prijs int constraint positieve_prijs check (prijs >= 0),
+	quota int constraint positief_quota check (quota >= 0), /* Is dit limiet of doel? */
+	is_weez bool default true,
+	_laatste_sync timestamp default NOW()
+	
+); /* Meerdere tarieven worden gebruikt oa voor bepalen van een activiteit */
+
+create table evenement (
+	id varchar primary key,
+	titel varchar not null,
+	beschrijving varchar,
+	starttijd timestamp not null,
+	eindtijd timestamp not null,
+	locatie varchar, /* Naam van de locatie */
+	straat varchar,
+	huisnummer varchar,
+	postcode varchar, /* Voorlopig geen zin in conversies */
+	stad varchar,
+	is_weez bool default true,
+	_laatste_sync timestamp default NOW(),
+	
+	categorie varchar references categorie(id),
+	
+	constraint einde_na_start check (eindtijd  > starttijd)
+	constraint adres_of_locatie check (locatie or (straat and huisnummer and postcode and stad)) /* Ofwel is er een adres of een locatie */
+);
+
+create table inschrijving (
+	evenement int references evenement(id) not null,
+	tarief varchar references tarief(id) not null,
+	lid varchar not null, /* id van lid - mag aangenomen worden dat dit altijd hex is? */
+	tijdstip timestamp default NOW(),
+	is_betaald bool default true,
+	is_geannuleerd bool default false,
+	is_terugbetaald bool default false,
+	opmerking varchar,
+	is_weez bool default true,
+	_laatste_sync timestamp default NOW(),
+	
+	primary key (evenement, lid) /* Staat 1 inschrijving per lid toe? */
+);
+
+create table evenement_tarief (
+	evenement int references evenement(id) not null,
+	tarief varchar references tarief(id) not null,
+	is_weez bool default true,
+	_laatste_sync timestamp default NOW(),
+	
+	primary key (evenement, tarief)
+);
+
