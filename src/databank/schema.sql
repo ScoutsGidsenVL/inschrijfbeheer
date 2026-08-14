966083c188f785f23d92edb5711242d19e1a3dc6
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-08-14 10:36:30 +0200

    feat: opzetten eerste versie databank

--- src/databank/schema.sql
+++ src/databank/schema.sql
@@ -1,69 +1,98 @@
 create table categorie (
 	id varchar primary key,
 	naam varchar unique not null, /* Zou uniek moeten zijn */
-	alt_naam varchar,
-	
-	_is_weez bool default true,
-	_laatste_sync timestamp default NOW()
-	
+	alt_naam varchar
 );
 
-create table tarief (
-	id varchar primary key,
-	evenement int references evenement(id) not null,
-	naam varchar, /* Potentieel constraint met ID dat combo uniek is */
-	prijs int constraint positieve_prijs check (prijs >= 0),
-	quota int constraint positief_quota check (quota >= 0 or quota is null), /* Is dit limiet of doel? */
-	starttijd_inschrijving timestamp default NOW(),
-	eindtijd_inschrijving timestamp default starttijd,
-	_is_weez bool default true,
-	_laatste_sync timestamp default NOW()
+create table evenement_status (
+	id serial primary key,
+	beschrijving varchar not null
+);
 
-	constraint einde_na_start check (eindtijd_inschrijving  > starttijd_inschrijving)
+insert into evenement_status values (1, 'Actief'), (2, 'Geannuleerd');
+
+create table locatie (
+	id serial primary key,
+	naam varchar,
+	straat varchar,
+	huisnummer varchar,
+	postcode varchar, /* TODO: bepalen waar omzetting int-string gebeurt */
+	stad varchar,
 	
-); /* Meerdere tarieven worden gebruikt oa voor bepalen van een activiteit */
+	constraint naam_of_adres check (
+		(naam is not null) 
+		or (
+			(straat is not null) 
+			and (huisnummer is not null) 
+			and (postcode is not null) 
+			and (stad is not null)
+		)
+	)
+);
 
 create table evenement (
 	id varchar primary key,
 	titel varchar not null,
-	status varchar,
+	status int default 1,
 	beschrijving varchar,
-	locatie varchar, /* Naam van de locatie */
-	straat varchar,
-	huisnummer varchar,
-	postcode varchar, /* Voorlopig geen zin in conversies */
-	stad varchar,
+	locatie int not null,
+	
 	starttijd timestamp not null,
 	eindtijd timestamp not null,
-	constraint einde_na_start check (eindtijd  > starttijd)
-	min_deelnemers int constraint positief_min check (min_deelnemers >= 0),
-	max_deelnemers int constraint positief_max check (max_deelnemers >= 0 and max_deelnemers >= min_deelnemers),
-	is_geannuleerd bool default false, /* TODO: cascade naar inschrijvingen */
-	aantal_zelfde_groep int constraint positief_aantal check (aantal_zelfde_groep >= 0 or aantal_zelfde_groep is null), /* Aantal inschrijvingen van dezelfde groep toegestaan */
-	min_leeftijd int constraint positief_min_leeftijd check (min_leeftijd >= 0 or min_leeftijd is null),
-	_is_weez bool default true,
-	_laatste_sync timestamp default NOW(),
 	
-	categorie varchar references categorie(id),
+	min_deelnemers int, 
+	max_deelnemers int,
+
+	aantal_zelfde_groep int,
 	
-	constraint einde_na_start check (eindtijd  > starttijd)
-	constraint adres_of_locatie check (locatie or (straat and huisnummer and postcode and stad)) /* Ofwel is er een adres of een locatie */
+	min_leeftijd int,
+
+	categorie varchar,
+	constraint positief_min check (min_deelnemers >= 0),
+	constraint positief_max check (max_deelnemers >= 0 and max_deelnemers >= min_deelnemers),
+	constraint positief_aantal check (aantal_zelfde_groep >= 0 or aantal_zelfde_groep is null),
+	constraint positief_min_leeftijd check (min_leeftijd >= 0 or min_leeftijd is null),
+	constraint einde_na_start check (eindtijd  > starttijd),
+
+	constraint fk_evenement_status foreign key (status) references evenement_status (id) on delete set null,
+	constraint fk_evenement_categorie foreign key (categorie) references categorie (id) on delete set null,
+	constraint fk_evenement_locatie foreign key (locatie) references locatie (id) on delete restrict
+);
+
+create table deelnemertype (
+	id varchar primary key,
+	evenement varchar not null,
+	naam varchar, /* Potentieel constraint met ID dat combo uniek is */
+	prijs int,
+	constraint positieve_prijs check (prijs >= 0),
+	quota int,
+	constraint positief_quota check (quota >= 0 or quota is null), /* Is dit limiet of doel? */
+	starttijd_inschrijving timestamp not null,
+	eindtijd_inschrijving timestamp not null,
+
+	constraint einde_na_start check (eindtijd_inschrijving  > starttijd_inschrijving),
+	
+	constraint fk_deelnemertype_evenement foreign key (evenement) references evenement (id) on delete cascade
+	
 );
 
 create table inschrijving (
-	evenement int references evenement(id) not null,
-	tarief varchar references tarief(id) not null,
+	evenement varchar not null,
+	deelnemertype varchar not null,
 	lid varchar not null, /* id van lid - mag aangenomen worden dat dit altijd hex is? */
 	tijdstip timestamp default NOW(),
 	is_betaald bool default true,
 	is_geannuleerd bool default false,
 	is_terugbetaald bool default false,
-	opmerking varchar,
-	vegetarisch bool default false,
+	/* TODO: maak eigen tabel voor bron van datas
 	_is_weez bool default true,
 	_laatste_sync timestamp default NOW(),
+	*/
 	
-	primary key (evenement, lid) /* Staat 1 inschrijving per lid toe? */
+	primary key (evenement, lid), /* Staat 1 inschrijving per lid toe? */
+	
+	constraint fk_inschrijving_evenement foreign key (evenement) references evenement (id) on delete restrict,
+	constraint fk_inschrijving_tarief foreign key (deelnemertype) references deelnemertype (id) on delete restrict
 
 	/*
 	gouw varchar not null,
@@ -73,12 +102,4 @@ create table inschrijving (
 	module_nummer varchar not null
 	
 	*/
-);
-
-create table inschrijving_vraag (
-	inschrijving int references inschrijving(evenement) not null,
-	vraag varchar not null,
-	antwoord varchar,
-	
-	primary key (inschrijving, vraag) /* Betere key zoeken */
 );
