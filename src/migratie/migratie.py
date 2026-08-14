c0f18a904351268f6559b2646e9d015c575eb3d9
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-08-14 13:45:39 +0200

    fix: migratie.py heeft geen onbestaande imports meer

--- src/migratie/migratie.py
+++ src/migratie/migratie.py
@@ -1,6 +1,4 @@
-from connectie import *
 from modellen import *
-from query_mapper import *
 
 import os
 from dotenv import load_dotenv
@@ -12,12 +10,4 @@ GEBRUIKERSNAAM = os.getenv("DB_username")
 WACHTWOORD = os.getenv("DB_password")
 
 if __name__ == "__main__":
-
-    cat = Categorie("testcat", "test categorie")
-
-    connectie = maak_connectie(HOST, DB_NAAM, GEBRUIKERSNAAM, WACHTWOORD)
-    print(connectie)
-
-    voer_query_uit(connectie, "INSERT INTO categorie (id, naam, alt_naam) VALUES (%s, %s, %s);", maak_categorie_query_arg(cat))
-
-    sluit_connectie(connectie)
+    pass
