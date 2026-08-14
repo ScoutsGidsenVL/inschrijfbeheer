f932540dbccca1216bc9ffbff3fb539f90ee7634
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-08-14 12:26:21 +0200

    feat: Inschrijving model in orde

--- src/migratie/modellen.py
+++ src/migratie/modellen.py
@@ -1,56 +1,67 @@
-from dataclasses import dataclass
-from datetime import datetime
+from django.db import models
 
-@dataclass
-class Inschrijving:
-    evenement: str
-    lid: str
-    deelnemertype: str
-    tijdstip: datetime
-    is_betaald: bool
-    is_geannuleerd: bool
-    is_terugbetaald: bool
+# @dataclass
+# class Evenement:
+#     id: str
+#     titel: str
+#     beschrijving: str
+#     status: int
+#     locatie: int
+#     starttijd: datetime
+#     eindtijd: datetime
+#     min_deelnemers: int
+#     max_deelnemers: int
+#     aantal_zelfde_groep: int
+#     min_leeftijd: int
+#     categorie: str # verwijst naar Categorie
 
-@dataclass
-class DeelnemerType:
-    id: str
-    evenement: str
-    naam: str
-    prijs: int
-    quota: int
-    starttijd_inschrijving: datetime
-    eindtijd_inschrijving: datetime
+class Lid(models.Model):
+    pass
 
-@dataclass
-class Evenement:
-    id: str
-    titel: str
-    beschrijving: str
-    status: int
-    locatie: int
-    starttijd: datetime
-    eindtijd: datetime
-    min_deelnemers: int
-    max_deelnemers: int
-    aantal_zelfde_groep: int
-    min_leeftijd: int
-    categorie: str # verwijst naar Categorie
+class Evenement(models.Model):
+    pass
 
-@dataclass
-class Categorie:
-    id: str
-    naam: str
-    alt_naam: str = None
+# @dataclass
+# class DeelnemerType:
+#     id: str
+#     evenement: str
+#     naam: str
+#     prijs: int
+#     quota: int
+#     starttijd_inschrijving: datetime
+#     eindtijd_inschrijving: datetime
 
-@dataclass
-class EvenementStatus:
-    id: int
-    beschrijving: str
+class DeelnemerType(models.Model):
+    pass
 
-@dataclass
-class Locatie:
-    id: int
-    naam: str = None
-    huisnummer: str = None
-    postcode: str = None
-    stad: str = None
+class Inschrijving(models.Model):
+    evenement = models.ForeignKey(Evenement, on_delete=models.RESTRICT)
+    lid = models.ForeignKey(Lid, on_delete=models.RESTRICT)
+    deelnemertype = models.ForeignKey(DeelnemerType, on_delete=models.RESTRICT)
+    tijdstip = models.DateField()
+    is_betaald = models.BooleanField()
+    is_geannuleerd = models.BooleanField()
+    is_terugbetaald = models.BooleanField()
+
+    class Meta:
+        app_label = "Integreat_migratie"
+        db_table = "inschrijving" # geeft naam van de tabel in de nieuwe databank aan
+
+# @dataclass
+# class Categorie:
+#     id: str
+#     naam: str
+#     alt_naam: str = None
+
+# @dataclass
+# class EvenementStatus:
+#     id: int
+#     beschrijving: str
+
+# @dataclass
+# class Locatie:
+#     id: int
+#     naam: str = None
+#     huisnummer: str = None
+#     postcode: str = None
+#     stad: str = None
