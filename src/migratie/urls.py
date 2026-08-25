cc2544cc75f383f9caca4393273aabcf2b1f8b6c
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-08-25 21:32:47 +0200

    refactor: deelnemers naar inschrijvingen

--- src/migratie/urls.py
+++ src/migratie/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("evenementen/", views.evenement_lijst, name="evenement_lijst"),
     path("evenementen/<str:id>/", views.evenement_detail, name="evenement_detail"),
-    path("evenementen/<str:id>/deelnemers/", views.evenement_deelnemers, name="evenement_deelnemers")
+    path("evenementen/<str:id>/inschrijvingen/", views.evenement_inschrijvingen, name="evenement_inschrijvingen")
 ]
--- src/migratie/views.py
+++ src/migratie/views.py
@@ -49,7 +49,7 @@ def evenement_detail(request: HttpRequest, id: str) -> HttpResponse:
         "evenement": evenement
     })
 
-def evenement_deelnemers(request: HttpRequest, id:str) -> HttpResponse:
+def evenement_inschrijvingen(request: HttpRequest, id:str) -> HttpResponse:
     return render(request, "evenementen/evenementen_deelnemers.html", {
         "value": "oeps"
     })
