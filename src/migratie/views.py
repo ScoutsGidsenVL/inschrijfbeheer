c39d11b9657963947acdfd06b3b2962527ddc3dc
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-08-26 16:29:56 +0200

    feat: namen worden getoond bij deelnemers

--- src/migratie/views.py
+++ src/migratie/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from .models import Evenement, Inschrijving
 from django.contrib.auth.decorators import login_required
+from .utils.soap import haal_lidgegevens
 
 KOLOMMEN = {
     "titel": "Titel",
@@ -76,11 +77,15 @@ def deelnemers_lijst(request: HttpRequest) -> HttpResponse:
     zoekterm = request.GET.get("q", '')
     deelnemers = Inschrijving.objects.values("lid").filter(lid__icontains=zoekterm).distinct() # Eventueel aanpassen naar apart model
 
+    deelnemers = [haal_lidgegevens(lid_id["lid"]) for lid_id in deelnemers]
+
     return render(request, "deelnemers/deelnemers_lijst.html", {
         "deelnemers": deelnemers
     })
 
 def deelnemers_detail(request: HttpRequest, id: str) -> HttpResponse:
+    deelnemer = haal_lidgegevens(id)
+
     return render(request, "deelnemers/deelnemers_detail.html", {
-        "deelnemer": id
+        "deelnemer": deelnemer
     })
