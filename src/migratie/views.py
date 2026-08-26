85901f3dd1d1789d43e3fe7b8fd46d5d8208978e
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-08-26 13:45:12 +0200

    feat: deelnemer detail werkt

--- src/migratie/views.py
+++ src/migratie/views.py
@@ -82,5 +82,5 @@ def deelnemers_lijst(request: HttpRequest) -> HttpResponse:
 
 def deelnemers_detail(request: HttpRequest, id: str) -> HttpResponse:
     return render(request, "deelnemers/deelnemers_detail.html", {
-        
+        "deelnemer": id
     })
