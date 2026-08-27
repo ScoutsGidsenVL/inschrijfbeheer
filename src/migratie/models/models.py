e45f6d9535dc5fcafd3bec8dae820e6bd2199346
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-08-27 16:49:06 +0200

    fix: verwijderen van onbestaande modellen
    In commentaar gezet voor eventueel later gebruik

--- src/migratie/models/models.py
+++ src/migratie/models/models.py
@@ -349,12 +349,12 @@ class IntegreatSalesInvoicedetail(models.Model):
         oid (str): object identifier
     """
     oid = models.BigIntegerField(db_column='OID', primary_key=True)
-    header = models.ForeignKey('IntegreatSalesInvoice', models.DO_NOTHING, db_column='Header', blank=True, null=True) # ticketinfo
-    article = models.ForeignKey('IntegreatArticle', models.DO_NOTHING, db_column='Article', blank=True, null=True)  # Artikel is steeds een vorming
+    # header = models.ForeignKey('IntegreatSalesInvoice', models.DO_NOTHING, db_column='Header', blank=True, null=True) # ticketinfo
+    # article = models.ForeignKey('IntegreatArticle', models.DO_NOTHING, db_column='Article', blank=True, null=True)  # Artikel is steeds een vorming
     unitprice = models.DecimalField(db_column='UnitPrice', max_digits=18, decimal_places=4, blank=True, null=True)  # BELANGRIJK BEVAT BETAALD BEDRAG
-    participant = models.ForeignKey('IntegreatParticipant', models.DO_NOTHING, db_column='Participant', blank=True, null=True)  # afleidbaar
-    seminar = models.ForeignKey('IntegreatSeminar', models.DO_NOTHING, db_column='Seminar', blank=True, null=True)  # afleidbaar
-    registration = models.ForeignKey('IntegreatRegistration', models.DO_NOTHING, db_column='Registration', blank=True, null=True)  # BELANGRIJK BEVAT INSCHRIJVING
+    participant = models.ForeignKey(IntegreatParticipant, models.DO_NOTHING, db_column='Participant', blank=True, null=True)  # afleidbaar
+    seminar = models.ForeignKey(IntegreatSeminar, models.DO_NOTHING, db_column='Seminar', blank=True, null=True)  # afleidbaar
+    registration = models.ForeignKey(IntegreatRegistration, models.DO_NOTHING, db_column='Registration', blank=True, null=True)  # BELANGRIJK BEVAT INSCHRIJVING
     createdon = models.DateTimeField(db_column='CreatedOn', blank=True, null=True)  # Wanneer betaald werd??
     nettoamount = models.DecimalField(db_column='NettoAmount', max_digits=18, decimal_places=4, blank=True, null=True)  # duplicate voor bedrag
     totalamount = models.DecimalField(db_column='TotalAmount', max_digits=18, decimal_places=4, blank=True, null=True)  # duplicate voor bedrag
