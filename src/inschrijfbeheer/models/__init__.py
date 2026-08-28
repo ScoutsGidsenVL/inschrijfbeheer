d95c76941692fca0ab39c8c367b19c7e1e519a36
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-08-28 16:13:38 +0200

    refactor + docs: splitsen van modellen in integreat en nieuwe + documentatie

--- src/inschrijfbeheer/models/__init__.py
+++ src/inschrijfbeheer/models/__init__.py
@@ -1,1 +1,27 @@
-from inschrijfbeheer.models.models import *
+from inschrijfbeheer.models.inschrijfbeheer_models import (
+    Lid,
+    Locatie,
+    EvenementStatus,
+    Categorie,
+    Evenement,
+    DeelnemerType,
+    Inschrijving,
+    EvenementVraagType,
+    EvenementVraag,
+    InschrijvingVraagAntwoord,
+)
+
+from inschrijfbeheer.models.integreat_models import (
+    IntegreatParticipantType,
+    IntegreatParticipant,
+    IntegreatSeminarStatus,
+    IntegreatSeminarType,
+    IntegreatCity,
+    IntegreatOrganisationUnit,
+    IntegreatOrganisationUnitSite,
+    IntegreatSeminar,
+    IntegreatRegistration,
+    IntegreatSeminarFreeFieldType,
+    IntegreatSeminarFreeField,
+    IntegreatRegistrationfreefield,
+)
