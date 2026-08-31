c99771c04c48c8ba211161fbd7cab7cbc356cf55
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-08-31 11:19:09 +0200

    feat: filter op Weez

--- src/inschrijfbeheer/views/evenementen_views.py
+++ src/inschrijfbeheer/views/evenementen_views.py
@@ -27,6 +27,7 @@ STANDAARD_KOLOMMEN = ["id", "titel", "status", "locatie", "is_weez"]
 def evenement_lijst(request: HttpRequest) -> HttpResponse:
     zoekterm: str = request.GET.get('q', '').strip()
     categorie_naam: str = request.GET.get('categorie', '').strip()
+    weez_filter: str = request.GET.get("weez", '')
     gekozen_kolommen = [k for k in request.GET.getlist("kolom") if k in KOLOMMEN]
     if not gekozen_kolommen:
         gekozen_kolommen = STANDAARD_KOLOMMEN
@@ -37,11 +38,16 @@ def evenement_lijst(request: HttpRequest) -> HttpResponse:
     )
 
     if categorie_naam:
-        print(categorie_naam)
         categorie = Categorie.objects.get(naam=categorie_naam)
         evenementen = evenementen.filter(
             categorie=categorie.id
         )
+
+    if weez_filter:
+        if weez_filter == '1':
+            evenementen = evenementen.filter(is_weez=True)
+        else:
+            evenementen = evenementen.exclude(is_weez=True)
 
     rijen = [
         (evenement.id, [getattr(evenement, kolom) for kolom in gekozen_kolommen])
