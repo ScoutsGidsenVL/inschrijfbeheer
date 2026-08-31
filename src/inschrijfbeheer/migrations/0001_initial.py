7810aa5b7aa17a2f1e02dc26730bea11583c5d69
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-08-31 15:54:02 +0200

    feat: opzetten sequence voor automatische id

---
+++ src/inschrijfbeheer/migrations/0001_initial.py
@@ -0,0 +1,17 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            "CREATE SEQUENCE IF NOT EXISTS evenement_vraag_id_seq;",
+            "DROP SEQUENCE IF EXISTS evenement_vraag_id_seq;",
+        ),
+        migrations.RunSQL(
+            "CREATE SEQUENCE IF NOT EXISTS inschrijving_vraagantwoord_id_seq;",
+            "DROP SEQUENCE IF EXISTS inschrijving_vraagantwoord_id_seq;",
+        ),
+    ]
