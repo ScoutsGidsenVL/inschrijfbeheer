bcaac6f9f67e964001928db2bc3c945be8ce7912
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-09-01 15:34:27 +0200

    feat: scraper checkt correct juiste velden

--- src/inschrijfbeheer/mapping/weez_mappers.py
+++ src/inschrijfbeheer/mapping/weez_mappers.py
@@ -7,13 +7,14 @@ op te halen.
 import logging
 import re
 from datetime import datetime
+from dataclasses import dataclass
 
 from inschrijfbeheer.models import (
     Categorie,
     Evenement,
     Locatie,
     Inschrijving,
-    Lid,
+    Deelnemer,
     EvenementVraag,
     InschrijvingVraagAntwoord
 )
@@ -59,14 +60,57 @@ def _split_adres(adres: str | None) -> tuple[str | None, str | None]:
     huisnummer, straat = match.groups()
     return huisnummer, straat
 
+@dataclass
+class InschrijvingsGegevens:
+    lidnummer: str = ''
+    voornaam: str = ''
+    achternaam: str = ''
+    mailadres: str = ''
 
-def bepaal_lidnummer(json: str) -> str:
+def bepaal_lidnummer(json: str) -> InschrijvingsGegevens | None:
+    gegevens = InschrijvingsGegevens()
+    aantal = 0
     for vraag_json in json:
-        if vraag_json.get("label") == "Lidnummer":
-            lidnummer = vraag_json.get("value")
-            if lidnummer:
-                return lidnummer
-    raise ValueError(f"Geen lidnummer gevonden in de vragen: {json}")
+        match vraag_json.get("label").lower():
+            case  "lidnummer":
+                lidnummer: str = vraag_json.get("value")
+                if lidnummer:
+                    gegevens.lidnummer = lidnummer
+                    aantal += 1
+            case "nom":
+                naam: str = vraag_json.get("value")
+                if naam:
+                    gegevens.achternaam = naam
+                    aantal += 1
+            case  "prenom":
+                voornaam: str = vraag_json.get("value")
+                if voornaam:
+                    gegevens.voornaam = voornaam
+                    aantal += 1
+            case  "email":
+                mail: str = vraag_json.get("value")
+                if mail:
+                    gegevens.mailadres = mail
+                    aantal += 1
+    if aantal == 4:
+        return gegevens
+    return None
+
+def valideer_lidnummer(lidnummer: str) -> bool:
+    return (
+        lidnummer.isnumeric()
+        and len(lidnummer) > 12
+        and len(lidnummer) < 15 
+    )
+
+def check_verplichte_vragen(vragen) -> bool:
+    verplichte_vragen = {"lidnummer", "nom", "prenom", "email"}
+    found_labels = set()
+    for vraag_json in vragen:
+        label = vraag_json.get("label")
+        if label:
+            found_labels.add(label.lower())
+    return verplichte_vragen.issubset(found_labels)
 
 
 class WeezSyncer(Mapper):
@@ -85,7 +129,6 @@ class WeezSyncer(Mapper):
 
     def __synchroniseer_evenementen(self) -> SynchronisatieInfo:
         overzicht = doe_weez_get(self.sessie, "events", parameters={
-            "include_closed": "1", 
             "include_without_sales": "1",
         })
 
@@ -95,7 +138,7 @@ class WeezSyncer(Mapper):
 
         for event in weez_event_lijst:
             event_id = event["id"]
-            self.synchroniseer_evenement(event_id)
+            self.synchroniseer_evenement(event_id, sync_deelnemers=True)
         return self.info
 
 
@@ -159,31 +202,33 @@ class WeezSyncer(Mapper):
 
         weez_deelnemers = doe_weez_get(self.sessie, f"participant/list", parameters={
             "id_event[]": evenement.id,
-            "include_deleted": "1",
             "full": "1",
         })
 
         weez_deelnemers = weez_deelnemers.get("participants", [])
 
         for deelnemer in weez_deelnemers:
+            vragen = deelnemer.get("answers")
+            if not check_verplichte_vragen(vragen):
+                self.__geen_verplichte_vraag(evenement)
+                break
+
             inschrijving_id = str(deelnemer.get("id_participant")) + str(deelnemer.get("id_event"))
             tarief_id = deelnemer.get("id_ticket")
-            vragen = deelnemer.get("answers")
-            lidnummer = bepaal_lidnummer(vragen)
-            lid_obj = self.__haal_of_maak_lid(lidnummer)
+            inschrijving_gegevens = bepaal_lidnummer(vragen)
+            lid_obj = self.__haal_of_maak_lid(inschrijving_gegevens)
         
             inschrijving, aangemaakt = Inschrijving.objects.update_or_create(
                 evenement=evenement,
                 lid=lid_obj,
                 defaults={
-                    "id":inschrijving_id,
                     "tijdstip":_parse_datetime(deelnemer.get('create_date')),
                     "is_weez":True,
                     "prijs": evenement_prijzen[tarief_id],
                 },
             )
 
-            self.synchroniseer_vragen(vragen=vragen, evenement=evenement, inschrijving=inschrijving)
+            self.synchroniseer_vragen(data=vragen, evenement=evenement, inschrijving=inschrijving)
             
             if aangemaakt:
                 self.info.registreer(Inschrijving, SynchronisatieActie.AANGEMAAKT)
@@ -195,6 +240,9 @@ class WeezSyncer(Mapper):
     def synchroniseer_vragen(self, evenement: Evenement | None = None, inschrijving: Inschrijving | None = None, data: dict | None = None) -> SynchronisatieInfo:
         if not data:
             return
+        if evenement and evenement.foutboodschap:
+            self.__geen_verplichte_vraag(evenement)
+        
         for index, vraag_json in enumerate(data):
             vraag, aangemaakt = EvenementVraag.objects.update_or_create(
                 evenement=evenement,
@@ -280,16 +328,47 @@ class WeezSyncer(Mapper):
             return categorie
         return None
 
-    def __haal_of_maak_lid(self, lidnummer: str) -> Lid:
-        lidgegevens = haal_lidgegevens(lidnummer) # TODO: check voornaam, achternaam, mail, geboortedatum, gsm (3/5) -> niet geldig = indicatie
-        lid, aangemaakt = Lid.objects.get_or_create(id=lidgegevens.id, defaults={
-            "voornaam": lidgegevens.voornaam,
-            "achternaam": lidgegevens.naam,
-            "mailadres": lidgegevens.emailadres,
-        })
+    def __haal_of_maak_lid(self, gegevens: InschrijvingsGegevens) -> Deelnemer:
+        if not valideer_lidnummer(gegevens.lidnummer):
+            self.info.registreer(Deelnemer, SynchronisatieActie.AANGEMAAKT)
+            
+            return Deelnemer.objects.create(
+                voornaam=gegevens.voornaam,
+                achternaam=gegevens.achternaam,
+                mailadres=gegevens.mailadres,
+                foutboodschap=f"Ongeldig lidnummer ingegeven: {gegevens.lidnummer}"
+            )
+        try:
+            lidgegevens = haal_lidgegevens(gegevens.lidnummer)
+            if (lidgegevens.voornaam == gegevens.voornaam
+                 or lidgegevens.naam == gegevens.achternaam
+                ):
+                self.info.registreer(Deelnemer, SynchronisatieActie.AANGEMAAKT)
+                 
+                return Deelnemer.objects.create(
+                    voornaam=gegevens.voornaam,
+                    achternaam=gegevens.achternaam,
+                    mailadres=gegevens.mailadres,
+                    foutboodschap=f"Onvoldoende matchende velden in inschrijving: {gegevens.lidnummer}"
+                )
+        except Exception as fout:
+            return None
 
+        lid, aangemaakt = Deelnemer.objects.get_or_create(
+            id=lidgegevens.id,
+            defaults={
+                "voornaam": lidgegevens.voornaam,
+                "achternaam": lidgegevens.naam,
+                "mailadres": lidgegevens.emailadres,
+            },
+        )
         if aangemaakt:
-            self.info.registreer(Lid, SynchronisatieActie.AANGEMAAKT)
+            self.info.registreer(Deelnemer, SynchronisatieActie.AANGEMAAKT)
 
         return lid
 
+
+    def __geen_verplichte_vraag(self, evenement: Evenement) -> None:
+        self.logger.warning(f"Evenement {evenement.titel} ({evenement.id}) bevat één van de  verplichte vragen niet")
+        evenement.foutboodschap = "Evenement mist een verplichte vraag, inschrijvingen worden niet gesynchroniseerd"
+        evenement.save()
