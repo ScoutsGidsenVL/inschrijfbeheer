ba03343c240eb864b3eb886d81e4aff9c1a2b4be
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-09-07 10:48:59 +0200

    feat: correcte redirect

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,16 +5,15 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
 """
 from django.urls import path, include
-from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from django.views.static import serve
 from django.conf import settings
 
 from inschrijfbeheer.views import log_lijst
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="evenement_lijst"), name="home"),
     path("oidc/", include("mozilla_django_oidc.urls")),
-    path("", login_required(TemplateView.as_view(template_name="home.html")), name="home"),
     path("docs/", serve, {"document_root": settings.BASE_DIR / '..' / 'docs', "path": "README.md"}), # README te vinden via docs/
     path("docs/<path:path>", serve, {"document_root": settings.BASE_DIR / '..' / 'docs'}), # technische documentatie terug te vinden via /docs/<naam>
     path("logs/", log_lijst, name="log_lijst"),
