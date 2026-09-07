96519e5d126035a18a5124b21e308c36ac68ede1
Thorsten De Neve <tdn@scoutsengidsenvlaanderen.be>
2026-09-07 11:21:53 +0200

    feat: paginering van deelnemers

--- src/inschrijfbeheer/views/deelnemers_views.py
+++ src/inschrijfbeheer/views/deelnemers_views.py
@@ -15,6 +15,7 @@ from inschrijfbeheer.models import Inschrijving, Deelnemer
 
 from inschrijfbeheer.utils.soap import haal_lidgegevens
 from inschrijfbeheer.utils.auth import check_rollen
+from inschrijfbeheer.utils.paginering import pagineer
 
 
 
@@ -39,8 +40,11 @@ def deelnemers_lijst(request: HttpRequest) -> HttpResponse:
         | Q(mailadres__icontains=zoekterm)
     ).distinct()
 
+    deelnemers, querystring = pagineer(request, deelnemers)
+
     return render(request, "deelnemers/deelnemers_lijst.html", {
-        "deelnemers": deelnemers
+        "deelnemers": deelnemers,
+        "querystring": querystring,
     })
 
 
